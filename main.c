7c47b0767d18058384979d2f6c516e2b35f3867f
jin1028 <57580138+jin1028@users.noreply.github.com>
2019-11-10 12:17:53 +0900

    c input output

---
+++ main.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+printf("Hello");
+return 0;
+
+}
+
+
+/*
+int main()
+{float a;
+float b;
+scanf("%f",&a);
+scanf("%f",&b);
+printf("%.f/%.f=%f",a,b,a/b);
+
+        return 0;
+
+}
+
+*/
+
+
+/*
+int main()
+{
+long long int a;
+long long int b;
+scanf("%lld",&a);
+scanf("%lld",&b);
+printf("%lld*%lld=%lld",a,b,a*b);
+return 0;
+}
+
+
+
+int main()
+{
+int a;
+int b;
+scanf("%d",&a);
+scanf("%d",&b);
+printf("%d*%d=%d",a,b,a*b);
+return 0;
+}
+*/
+
+
+/*
+제목 : 기본 입출력
+작성자 : 진솔
+*/
+
+/*
+int main()
+{int a; //변수 a 선언
+int b;
+int c;
+
+//출력
+printf("첫번쨰 수를 입력해주세요.");
+scanf("%d",&a);
+printf("두번째 수를 입력해주세요.");
+scanf("%d",&b);
+printf("세번째 수를 입력해주세요.");
+scanf("%d",&c);
+
+printf("%d+%d+%d=%d",a,b,c,a+b+c);
+    return 0;
+}
+
+*/
